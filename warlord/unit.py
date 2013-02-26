feb4da1196e35ab6f115f00c9f4b5e0f764e92de
Mark McGuire <mark.b.mcg@gmail.com>
2013-02-26 00:41:06 +0000

    Simple pathing

--- warlord/unit.py
+++ warlord/unit.py
@@ -1,2 +1,21 @@
 class Unit(object):
+    def __init__(self):
+        self.location = (0, 0)
+
+class BadDirectionError(ValueError):
     pass
+
+def path(unit, path):
+    for step in path:
+        loc = unit.location
+        if step == 'U':
+            unit.location = (loc[0], loc[1] + 1)
+        elif step =='D':
+            unit.location = (loc[0], loc[1] - 1)
+        elif step == 'R':
+            unit.location = (loc[0] + 1, loc[1])
+        elif step =='L':
+            unit.location = (loc[0] - 1, loc[1])
+        else:
+            raise BadDirectionError
+
